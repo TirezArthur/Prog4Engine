29b1c9f92c043db9c09eb3837bef4ce2a26a512a
Tirez-Arthur <Arthur.tirez@gmail.com>
2025-03-31 08:52:16 +0200

    Engine is now a static library

--- Engine/Command.hpp
+++ Engine/Command.hpp
@@ -1,6 +1,4 @@
 #pragma once
-#include "LifeComponent.h"
-#include "ScoreComponent.h"
 #include <string>
 #include <iostream>
 #include <concepts>
@@ -40,21 +38,3 @@ private:
 	ValueType m_Modifier;
 };
 
-class DamageCommand : public Command
-{
-public:
-	DamageCommand(dae::LifeComponent* lifeTarget) : m_LifeComponent{ lifeTarget } {}
-	virtual void Execute() override { m_LifeComponent->LoseLife(); };
-private:
-	dae::LifeComponent* m_LifeComponent;
-};
-
-class ScoreCommand : public Command
-{
-public:
-	ScoreCommand(dae::ScoreComponent* scoreTarget, int score) : m_ScoreComponent{ scoreTarget }, m_Score{ score } {}
-	virtual void Execute() override { m_ScoreComponent->IncrementScore(m_Score); };
-private:
-	dae::ScoreComponent* m_ScoreComponent;
-	int m_Score;
-};
--- Engine/Hash.hpp
+++ Engine/Hash.hpp
@@ -3,7 +3,7 @@
 
 namespace dae {
     //Code provided in the slides, slightly modified
-
+    //TODO consteval
     //Hashes compile time if the argument is a literal or at runtime if the argument is a string or other object
     constexpr uint32_t sdbm_hash(const std::string_view text) {
         uint32_t hash = 0;
